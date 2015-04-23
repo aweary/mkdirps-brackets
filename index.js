a74ff50a4d1277e995c346ab55d409468b1af73a
Brandon Dail <Kierkegaurd@gmail.com>
2015-04-23 00:03:46 -0400

    * corrected failure detection logic * added optional options object argument * added optional onSuccess method, taking path as an argument * added option fileExists method, taking path as an argument

--- index.js
+++ index.js
@@ -3,9 +3,15 @@ var async  	= require('async');
 var fs		= require('fs');
 var mkdirp  = require('mkdirp');
 
-module.exports = function(path, superCallback){
+module.exports = function(path, options, superCallback){
+	
+	if (!path || typeof path !== 'string') throw new Error('No valid path(s) provided!');
 
-	if(!path || typeof path !== 'string') throw new Error('No valid path(s) provided!');
+	/* Dynamically check if we've been provided an option object, redefined argument parameters if so  */
+	if (arguments.length === 2) {
+		var superCallback = options;
+		var options = {};
+	};
 
 	var expandedPath = expand(path);
 
@@ -16,14 +22,19 @@ module.exports = function(path, superCallback){
 				var defined = fs.statSync(path, function(err, stats){ return true; });
 			}
 			/* We know that if there is an error, the path isn't defined */
-			catch (error) {
+			catch (err) {
 				var defined = false;
-			};	
-			
-			/* if the path is not defined, make it */
-			if(!defined) {
-				mkdirp(path, superCallback);
 			};
+
+			/* if the path is not defined, make it */
+			if (!defined) {
+				mkdirp(path, superCallback);
+				if(options.onSuccess) options.onSuccess(path);
+			}
+			/* Otherwise, call the folderExists optional method if it exists */
+			else {
+				if(options.folderExists) options.folderExists(path);
+			}
 			
 			callback(null);
 
