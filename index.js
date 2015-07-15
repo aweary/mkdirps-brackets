a779d2160181bb5ceeae2143261d620e7966a657
Brandon Dail <Kierkegaurd@gmail.com>
2015-07-15 17:25:36 -0500

    Error passes to callback if path is invalid

--- index.js
+++ index.js
@@ -5,21 +5,23 @@ var mkdirp = require('mkdirp');
 
 module.exports = function(path, _callback) {
 
-  if (!path || typeof path !== 'string') throw new Error('No valid path(s) provided!');
+  if (!path || typeof path !== 'string') {
+    _callback(new Error('No valid path(s) provided!'));
+    return;
+  }
 
   var expandedPath = expand(path);
   async.each(expandedPath, function(path, callback) {
 
     try {  var defined = fs.statSync(path, function(err, stats) { return true }); }
+
     catch (err) { var defined = false; };
 
-    /* if the path is not defined, make it */
     if (!defined) {
       mkdirp(path);
     }
 
     callback(null);
-
   });
 
   _callback(null);
