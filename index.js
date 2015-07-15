c9bb7c610c903c036505b1d2360785ec14ce1f5b
Brandon Dail <Kierkegaurd@gmail.com>
2015-07-15 17:19:57 -0500

    Simplify module API add .gitignore

--- index.js
+++ index.js
@@ -1,43 +1,27 @@
-var expand 	= require('brace-expansion');
-var async  	= require('async');
-var fs		= require('fs');
-var mkdirp  = require('mkdirp');
+var expand  = require('brace-expansion');
+var async = require('async');
+var fs = require('fs');
+var mkdirp = require('mkdirp');
 
-module.exports = function(path, options, superCallback){
-	
-	if (!path || typeof path !== 'string') throw new Error('No valid path(s) provided!');
+module.exports = function(path, _callback) {
 
-	/* Dynamically check if we've been provided an option object, redefined argument parameters if so  */
-	if (arguments.length === 2) {
-		var superCallback = options;
-		var options = {};
-	};
+  if (!path || typeof path !== 'string') throw new Error('No valid path(s) provided!');
 
-	var expandedPath = expand(path);
+  var expandedPath = expand(path);
+  async.each(expandedPath, function(path, callback) {
 
-	async.each(expandedPath, function(path, callback){
+    try {  var defined = fs.statSync(path, function(err, stats) { return true }); }
+    catch (err) { var defined = false; };
 
-			/* fs.statSync will throw an error if the file doesn't exist, so wrap in try/catch  */
-			try {
-				var defined = fs.statSync(path, function(err, stats){ return true; });
-			}
-			/* We know that if there is an error, the path isn't defined */
-			catch (err) {
-				var defined = false;
-			};
+    /* if the path is not defined, make it */
+    if (!defined) {
+      mkdirp(path);
+    }
 
-			/* if the path is not defined, make it */
-			if (!defined) {
-				mkdirp(path, superCallback);
-				if(options.onSuccess) options.onSuccess(path);
-			}
-			/* Otherwise, call the folderExists optional method if it exists */
-			else {
-				if(options.folderExists) options.folderExists(path);
-			}
-			
-			callback(null);
+    callback(null);
 
-	});
+  });
+
+  _callback(null);
 
 };
